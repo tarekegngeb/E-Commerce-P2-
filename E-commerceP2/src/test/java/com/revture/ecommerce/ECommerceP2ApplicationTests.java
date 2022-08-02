2b687cb322a40d800db9ef7f496bf05cd6c9e34a
Jerry Reeves <jerryjr0425@gmail.com>
2022-08-02 14:33:24 -0500

    Updated Project

---
+++ E-commerceP2/src/test/java/com/revture/ecommerce/ECommerceP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revture.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
